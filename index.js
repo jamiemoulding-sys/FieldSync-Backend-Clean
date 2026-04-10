53f0760d0412a23495509de10aa97b0630d41182
Jamie <jamiemoulding@outlook.com>
2026-04-10 16:37:55 +0100

    fix101

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+const fetch = require('node-fetch');
+
+global.fetch = fetch;
+global.Headers = fetch.Headers;
+global.Request = fetch.Request;
+global.Response = fetch.Response;
+
 require('dotenv').config();
 
 const express = require('express');
